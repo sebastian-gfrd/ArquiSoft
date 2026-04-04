3791fd2501533bab29478999d549355695013649
sebastian-gfrd <sc.martineza12@uniandes.edu.co>
2026-04-03 19:24:13 -0500

    Cambio al health checker

--- App_Web/App_Web/urls.py
+++ App_Web/App_Web/urls.py
@@ -18,11 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-def health_check(request):
-    return HttpResponse("OK", status=200)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('health/', health_check),
     path('', include('core.urls')),
 ]
