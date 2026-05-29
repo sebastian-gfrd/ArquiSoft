4bbcf904224011af9f59aeeb7e586318357a85e7
sebastian-gfrd <98659393+sebastian-gfrd@users.noreply.github.com>
2026-05-29 11:18:57 -0500

    Ajuste rutas

--- App_Web/App_Web/urls.py
+++ App_Web/App_Web/urls.py
@@ -1,20 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views # Ajusta según tus vistas
 
 urlpatterns = [
     # Envolvemos todo dentro del prefijo /auth/ para que coincida con el ALB
     path('auth/', include([
         path('admin/', admin.site.urls),
-        path('health/', views.health, name='health'),
-        path('login/', views.login, name='login'),
-        path('callback/', views.callback, name='callback'),
-        path('logout/', views.logout, name='logout'),
-        
-        # Tus APIs de tenants y proyectos
-        path('api/v1/tenants/', views.TenantListCreate.as_view(), name='tenant-list-create'),
-        path('api/v1/tenants/<int:pk>/', views.TenantDetail.as_view(), name='tenant-detail'),
-        path('api/v1/projects/', views.ProjectListCreate.as_view(), name='project-list-create'),
-        path('api/v1/projects/<int:pk>/', views.ProjectDetail.as_view(), name='project-detail'),
+        path('', include('core.urls')),
     ])),
 ]
